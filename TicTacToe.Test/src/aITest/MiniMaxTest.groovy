2ae8a6d6f1a21bb9f6c7225747b3a4657a2dd19d
Shawn Horvatic <shorvatic@pekininsurance.com>
2016-08-31 08:00:17 -0500

    [WIP]

--- TicTacToe.Test/src/aITest/MiniMaxTest.groovy
+++ TicTacToe.Test/src/aITest/MiniMaxTest.groovy
@@ -27,19 +27,10 @@ public class MiniMaxTest {
 		}
 	}
 	
-	@Test
-	void Play_Every_Possabile_Game() {
-		def startingBoard = new TicTacToeBoard()
-		def ai = new MiniMax();
-		for(humanRowStart in 0..2) {
-			for(humanColumnStart in 0..2) {
-				nextMove(startingBoard, humanRowStart, humanColumnStart, ai)
-			}
-		}
-	}
-
 	private nextMove(currentBoard, humanChoosenRow, humanChoosenColumn, aiBot) {
 		def boardWithMoves = currentBoard.setBoardPositon(humanChoosenRow, humanChoosenColumn, BoardValue.O)
+		if(!boardWithMoves)
+			return
 		if(gameOver(boardWithMoves)) {
 			testHumanDidNotWin(boardWithMoves)
 			return
@@ -49,36 +40,12 @@ public class MiniMaxTest {
 			testHumanDidNotWin(boardWithMoves)
 			return
 		}
-		def nextHumanChoosenRow, nextHumanChoosenColumn
-		(nextHumanChoosenRow, nextHumanChoosenColumn) = pickNextMove(boardWithMoves, humanChoosenRow, humanChoosenColumn)
-		nextMove(boardWithMoves, nextHumanChoosenRow, nextHumanChoosenColumn, aiBot)
+		for(nextHumanChoosenRow in 0..2) {
+			for(nextHumanChoosenColumn in 0..2) {
+				nextMove(boardWithMoves, nextHumanChoosenRow, nextHumanChoosenColumn, aiBot)
+			}
+		}
 	}
-
-	private pickNextMove(TicTacToeBoard currentBoard, humanChoosenRow, humanChoosenColumn) {
-		def possibleRowPick = humanChoosenRow
-		def possibleColumnPick = humanChoosenColumn
-		while( !(currentBoard.setBoardPositon(possibleRowPick, possibleColumnPick, BoardValue.O)) ) {
-			(possibleRowPick, possibleColumnPick) = setRowAndColumn(possibleRowPick, possibleColumnPick)
-		}
-		[possibleRowPick, possibleColumnPick]
-	}
-
-	private setRowAndColumn(humanChoosenRow, humanChoosenColumn) {
-		def possibleRowPick = humanChoosenRow
-		def possibleColumnPick = humanChoosenColumn
-		possibleColumnPick++;
-		if(possibleColumnPick + 1 > 2 && possibleRowPick + 1 > 2) {
-			return [0, 0]
-		}
-		if(possibleColumnPick > 2) {
-			possibleColumnPick = 0;
-			possibleRowPick++;
-		}
-		if(possibleRowPick > 2) {
-			possibleRowPick = 0;
-			possibleColumnPick++;
-		}
-		[possibleRowPick, possibleColumnPick]}
 
 	private gameOver(currentBoard) {
 		!CheckGame.currentGameStatus(currentBoard).is(VictoryValue.ONGOING)
